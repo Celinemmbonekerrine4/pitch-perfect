2a9648550ae0a7ba64de9adcfa045be036809765
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2020-02-18 15:03:14 +0300

    it will create a password hash for the user account

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
-from . import db
+from app.home import db
 
 from werkzeug.security import generate_password_hash,check_password_hash
 
-class User:
+class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
@@ -10,8 +10,28 @@ class User:
     pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
     pass_secure = db.Column(db.String(255))
 
+        @property
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
 
+        @password.setter
+        def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
 
 def __repr__(self):
     return f'User{self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255))
+
+def __repr__(self):
+    return f'User{self.name}'
+
 
