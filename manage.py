1ac1d2881ff69cfe6ceed68f966228a32a219593
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2020-02-18 14:40:02 +0300

    it will import the pitch class and pass it into the shell context

--- manage.py
+++ manage.py
@@ -16,7 +16,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User,Pitch=Pitch)
 
 if __name__ == '__main__':
 
