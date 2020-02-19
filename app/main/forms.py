c374eb72e4b10b0c93878d04c9d44d1dfa823ea7
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2020-02-19 08:59:57 +0300

    it will create the Login forms

---
+++ app/main/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm 
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
+
+
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
