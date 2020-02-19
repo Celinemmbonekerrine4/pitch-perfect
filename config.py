7c36fd9d229877b3caaab9c2b58f6d5e3671e73c
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2020-02-19 11:30:41 +0300

    it will add the email congigurations

--- config.py
+++ config.py
@@ -2,8 +2,13 @@ import os
 
 class Config:
     
-     SQL_ALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringaschool:Access@localhost/pitches'
-     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQL_ALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringaschool:Access@localhost/pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     pass
