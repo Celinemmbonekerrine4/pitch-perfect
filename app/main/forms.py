0bffbc216a616bcc4508f3be3280b3752878850a
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2020-02-19 10:01:06 +0300

    it will create an update profile class

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,16 +1,8 @@
 from flask_wtf import FlaskForm 
-from wtforms import StringField,PasswordField,BooleanField,SubmitField,ValidationError
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
-class RegistrationForm(FlaskForm):
-     def validate_email(self,data_field):
-            if User.query.filter_by(email =data_field.data).first():
-                raise ValidationError('There is an account with that email')
-
-    def validate_username(self,data_field):
-        if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('That username is taken')
 
 
 class LoginForm(FlaskForm):
@@ -18,3 +10,7 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators =[Required()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
