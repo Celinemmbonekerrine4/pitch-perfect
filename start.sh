cb0f6e557e4010ec689b2fb08b066d0b6159e33c
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2020-02-19 11:38:47 +0300

    it will export the mail username and password

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-export MAIL_USERNAME=<celinekerrine@gmail.com>
-export MAIL_PASSWORD=<celinemmbone3>
+export MAIL_USERNAME=celinekerrine@gmail.com
+export MAIL_PASSWORD=celinemmbone3
 python3.6 manage.py server
