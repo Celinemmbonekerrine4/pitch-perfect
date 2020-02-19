63d3c3d3570491e8f0dc5f660258d71504f5d27d
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2020-02-19 11:17:57 +0300

    it will config uploads and import the images

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+
+
+
 
 
 login_manager = LoginManager()
@@ -10,6 +14,7 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
 
 
 def create_app(config_name):
@@ -20,12 +25,19 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
 
+    #UploadSet config
+    configure_uploads(app,photos)
+
+    
      # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    
+
 
     return app
 
