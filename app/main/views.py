06e9219cfdc8e6eaa377ae523624cfedf90fd3d9
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2020-02-19 12:12:42 +0300

    it will update the pitch function

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
-from flask_login import login_user,logout_user,login_required
+from flask_login import login_user,logout_user,login_required,current_user
 from .forms import LoginForm,RegistrationForm,UpdateProfile
-from .. import db
+from .. import db,photos
 from ..models import User
 
 
@@ -10,8 +10,25 @@ from ..models import User
 # from .forms import ReviewForm
 # from ..models import Review
 
-@main.route('/')
-def index():
+@main.route('/pitch/new/<int:id>',methods=['GET','POST'])
+@login_required
+def new_pitch():
+    form = ReviewPitch()
+    pitch = get_pitch(id)
+    if form.validate_on_submit():
+        title = form.title.data
+        pitch = form.pitch.data
+
+        # Updated pitch instance
+        new_pitch = Pitch(pitch_id=pitch.id,pitch_title=title,image_path=pitch.poster,user=current_user)
+
+        # save pitch method
+        new_pitch.save_pitch()
+        return redirect(url_for('.pitch',id = pitch.id ))
+
+     title = 'Home - Get and Make a pitch'
+
+    return render_template(title = title, pitch_form=form, pitch=pitch)
     # pitch = get_pitch(id)
     # title = f'{pitch.title}'
 
@@ -20,9 +37,8 @@ def index():
     # interview_pitch = get_pitch('interview')
     # promotion_pitch = get_pitch('promotion')
 
-    title = 'Home - Get and Make a pitch'
-
-@main.route('/user/<uname>/update', methods = ['GET','POST'])
+   
+@main.route('/user/<uname>/update/pic', methods = ['GET','POST'])
 @login_required
 def update_profile(uname):
     user = User.query.filter_by(username = uname).first()
@@ -38,6 +54,15 @@ form = UpdateProfile()
         db.session.commit()
 
         return redirect(url_for('.profile',uname=user.username))
+        #pic route
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
 
     return render_template('profile/update.html',form =form)
 
