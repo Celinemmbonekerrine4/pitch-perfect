dfe18e583b1a6c4c898209e2aabdac43c48ea769
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2020-02-19 11:59:15 +0300

    it will update the pitch model

--- app/models.py
+++ app/models.py
@@ -2,14 +2,18 @@ from . import login_manager
 from flask_login import UserMixin
 from . import db
 from werkzeug.security import generate_password_hash
+from datetime import datetime
 
 class Pitch(db.Model):
 
    __tablename__ = 'pitches'
-   id = db.Column(db.Integer,primary_key = True)
-
-   name = db.Column(db.String(255))
-   users = db.relationship('User', backref = 'pitch', lazy='dynamic')
+  id = db.Column(db.Integer,primary_key = True)
+  pitch_id = db.Column(db.Integer)
+  pitch_title = db.Column(db.String)
+  image_path = db.Column(db.String)
+  pitch_review = db.Column(db.String)
+  posted = db.Column(db.DateTime,default=datetime.utcnow)
+  user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
 
    def  __init__(self,id,title,pitch,vote_average,vote_count):
     self.id = id
